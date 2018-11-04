a9592a34fc9bcf48d1f2e1c09eabb393c5c6ff8f
Jim Pick <jim@backblend.com>
2018-11-04 15:32:02 -0800

    Fix es default export

--- crel.js
+++ crel.js
@@ -34,7 +34,7 @@
 
 */
 
-(function (root, factory) {
+export default (function (root, factory) {
     if (typeof exports === 'object') {
         module.exports = factory();
     } else if (typeof define === 'function' && define.amd) {
